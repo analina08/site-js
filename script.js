6dbcae6cbfdaa389b33da9994ef32621229e82e7
analina08 <analinaxmota0808@gmail.com>
2024-12-17 17:42:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onscroll = function() 
+{
+    const scroll = document.getElementById("scroll")
+
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
+    {
+        scroll .style.display = "block";
+        
+    }
+    else{
+        scroll .style.display = "none";
+    }
+};
+document.getElementById("scroll").addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+function start() 
+{
+   const fimHora = new Date().getTime() +3600 * 1000;
+   const horaElemento = document.getElementById('tempo');
+
+   const interval = setInterval(function(){
+        const now = new Date().getTime();
+        const timeRegresso = fimHora - now;
+        if (timeRegresso <= 0) {
+            clearInterval(interval);
+            horaElemento.innerHTML = "A promoção acabou ;(";
+        }
+        else{
+            const hora = Math.floor(timeRegresso / (1000 * 60 * 60));
+            const minutos = Math.floor((timeRegresso % (1000 * 60 * 60)) / (1000 * 60));
+            const segundos = Math.floor((timeRegresso % (1000 * 60)) / 1000 * 60);
+
+            horaElemento.innerHTML = `${hora}h ${minutos}m ${segundos}s`;
+        }
+    }, 1000);
+}
+window.addEventListener('load', start);
+
+let testeModal = 0;
+function show() 
+{
+    const testemodal = document.querySelectorAll(".modais");
+    testemodal.forEach((modal, index) => {
+        modal.styl.display = (index === testeModal) ? 'block' : 'none';
+    });
+    testeModal = (testeModal + 1) % testemodal.length;
+}
+window.addEventListener('load', function(){
+    show();
+    setInterval(show, 3000)
+});
